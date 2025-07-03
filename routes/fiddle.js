286a808b8723163faba6d4a10f1be4a9ea8cc445
samndambuki <sam.ndambuki08@gmail.com>
2025-07-03 10:58:13 +0300

    worked on get requests endpoints for fiddle

--- routes/fiddle.js
+++ routes/fiddle.js
@@ -38,5 +38,49 @@ fiddles/ - DELETE
 //req - contains info about the request
 //res - contains info about the response
 router.get('/:fiddleid',(req,res)=>{
-    fiddleModel.findOne({fiddleid:req.params.fiddleid}).then()
+    //findOne - to search database
+    //looks for a single document where the fiddleid matches the req.params.fiddleid
+    //req.params.fiddleid extracts the fiddleid parameter from /fiddles/abc123   
+    fiddleModel.findOne({fiddleid:req.params.fiddleid})
+    //handles a successful case when the database query completes
+    //doc contains the found document
+    .then((doc)=>{
+        //sends a json response 
+        //error:false - indicates no error occured
+        //response:doc - actual fiddle data found
+        if(!doc){
+            return res.status(404).json({error:true,message:'fiddle not found'})
+        }
+        res.status(200).json({error:false,response:doc})
+    })
+    //handle any errors that occur during the database operation
+    .catch((error)=>{
+        //logs the error to the console
+        console.log(error);
+        res.status(500).json({error:true,message:"database error"});
+    })
+})
+
+/*
+/fiddles/user/user123
+*/
+//req.params.userid user123
+router.get('/user/:userid',(req,res)=>{
+    //finds all fiddles that belong to a specific user
+    fiddleModel.find({userid:req.params.userid}).then((docs)=>{
+        if(!docs){
+            return res.status(404).json({error:false,message:'no fiddles found'})
+        }
+        res.status(200).json({error:false,response:docs})
+    }).catch((error)=>{
+        console.log(error);
+        res.status(500).json({error:true,message:'database error'})
+    })
+})
+
+
+router.post('/',(req,res)=>{
+    let fiddleObj = req.body;
+    fiddleObj['fiddleid'] = uuidv4();
+    let newFiddle = new fiddleModel.new(fiddleObj)
 })
